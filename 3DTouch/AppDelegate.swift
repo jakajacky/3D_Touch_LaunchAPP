ea4b26a094c99ea2b54ab2af3f88c0f519d40607
xqzh <xqzh@ideabinder.com>
2016-09-08 15:31:37 +0800

    first commit

--- 3DTouch/AppDelegate.swift
+++ 3DTouch/AppDelegate.swift
@@ -16,6 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
+    let addIcon   = UIApplicationShortcutIcon(type: UIApplicationShortcutIconType.Add)
+    let alarmIcon = UIApplicationShortcutIcon(type: UIApplicationShortcutIconType.Alarm)
+    let cloudIcon = UIApplicationShortcutIcon(type: UIApplicationShortcutIconType.Cloud)
+    let bookMIcon = UIApplicationShortcutIcon(type: UIApplicationShortcutIconType.Bookmark)
+    // 创建一个标签，并配置相关属性。
+    let addItem = UIApplicationShortcutItem(type: "a", localizedTitle: "添加", localizedSubtitle: "", icon: addIcon, userInfo: nil)
+    let alarmItem = UIApplicationShortcutItem(type: "b", localizedTitle: "闹钟", localizedSubtitle: "", icon: alarmIcon, userInfo: nil)
+    let cloudItem = UIApplicationShortcutItem(type: "c", localizedTitle: "cloud", localizedSubtitle: "", icon: cloudIcon, userInfo: nil)
+    let booMItem = UIApplicationShortcutItem(type: "d", localizedTitle: "书签", localizedSubtitle: "", icon: bookMIcon, userInfo: nil)
+    // 将标签添加进Application的shortcutItems中。
+    UIApplication.sharedApplication().shortcutItems = [addItem, alarmItem, cloudItem, booMItem];
     return true
   }
 
@@ -39,6 +50,57 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  }
+  
+  func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) {
+    print("\(shortcutItem.localizedTitle)")
+    if shortcutItem.type == "b" {
+      let tab = window!.rootViewController! as! UITabBarController
+      // 判断：要推出的vc位于tabviewcontroller的哪一个tab
+      if tab.selectedIndex != 0 {
+        tab.selectedIndex = 0
+      }
+      
+      let viewc = window!.rootViewController!.childViewControllers[0] as! UINavigationController
+      if viewc.topViewController?.restorationIdentifier != "navi_vc_3d" {
+        viewc.popViewControllerAnimated(false)
+      }
+      
+      let story = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+      let vc = story.instantiateViewControllerWithIdentifier("3d")
+      
+      if viewc.topViewController?.restorationIdentifier == "com.3dtouch.vc" {
+        // 判断：如果栈顶控制器正是3D-Touch需要弹出的VC，不需要重新push
+      }
+      else if viewc.topViewController?.restorationIdentifier == "navi_vc_3d"{
+        viewc.pushViewController(vc, animated: true)
+      }
+    }
+    else if shortcutItem.type == "d" {
+      let tab = window!.rootViewController! as! UITabBarController
+      // 判断：要推出的vc位于tabviewcontroller的哪一个tab
+      if tab.selectedIndex != 0 {
+        tab.selectedIndex = 0
+      }
+      
+      let viewc = window!.rootViewController!.childViewControllers[0] as! UINavigationController
+      if viewc.topViewController?.restorationIdentifier != "navi_vc_3d" {
+        viewc.popViewControllerAnimated(false)
+      }
+      
+      let story = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+      let vc = story.instantiateViewControllerWithIdentifier("3d1")
+      
+      if viewc.topViewController?.restorationIdentifier == "3d1" {
+        // 判断：如果栈顶控制器正是3D-Touch需要弹出的VC，不需要重新push
+      }
+      else if viewc.topViewController?.restorationIdentifier == "navi_vc_3d" {
+        viewc.pushViewController(vc, animated: true)
+      }
+     
+    }
+    
+    completionHandler(true)
   }
 
 
